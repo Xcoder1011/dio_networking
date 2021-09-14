3a1e3627ab7d90dbc365b0da766f7da5250d9f5e
wushangkun <wushangkun@chelun.com>
2021-09-14 11:20:21 +0800

    处理FlutterError.onError回调的异常

--- dio_networking/example/lib/crash_report.dart
+++ dio_networking/example/lib/crash_report.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:isolate';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
@@ -14,6 +15,21 @@ class CrashReport {
       const MethodChannel('flutter_app_native');
 
   static runCrashGuarded<R>(R body()) {
+    // 处理FlutterError.onError回调的异常
+    FlutterError.onError = (FlutterErrorDetails details) {
+      // 将FlutterError的异常处理中转至runZoned的异常处理handler中, 因为两者的处理方式是一样的, 如果处理方式不一样, 可以分开来处理
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    };
+
+    // 处理Isolate回调的异常, 只针对main isolate或者root isolate生效
+    // 通过Isolate.spawn()创建的isolate中的异常需要调用方自己捕获
+    // 不过目前还没有发现什么操作才会在这里回调异常
+    Isolate.current.addErrorListener(RawReceivePort((dynamic pair) {
+      List<dynamic> errors = pair as List<dynamic>;
+      // 同样, Isolate中的异常也中转至runZoned的异常处理handler中
+      Zone.current.handleUncaughtError(errors.first, errors.last);
+    }).sendPort);
+
     runZonedGuarded(() => body(), (error, stack) {
       var details = _makeErrorDetails(error, stack);
       _reportErrorAndLog(details);
